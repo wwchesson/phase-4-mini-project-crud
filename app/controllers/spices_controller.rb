8fe449bf853eb004a9a2aa13f39ef2cee5022cd2
Will <willchesson@gmail.com>
2022-07-01 14:49:12 -0400

    commit tree 05cd185b4c2ee8c6e1cc5d8b793459d2e8096362

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,35 @@
+class SpicesController < ApplicationController
+    
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = find_spice
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    def destroy
+        spice = find_spice
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def find_spice
+        Spice.find(params[:id])
+    end
+
+end
